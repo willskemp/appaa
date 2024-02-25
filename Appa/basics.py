f4425e53f5dc137bbe76bea353436955847faa79
willskemp <willskemp@gmail.com>
2024-02-25 20:52:23 +0000

    Create difference in means class

--- Appa/basics.py
+++ Appa/basics.py
@@ -1,12 +1,17 @@
 import numpy as np
 
 
-def average_treatment_effect_point_estimate(obs_test, obs_control):
-    return np.mean(obs_test) - np.mean(obs_control)
+class DifferenceInMeans:
+    def __init__(self, obs_test, obs_control):
+        self.obs_test = obs_test
+        self.obs_control = obs_control
 
+    def est(self):
+        return np.mean(self.obs_test) - np.mean(self.obs_control)
 
-def average_treatment_effect_variance(obs_test, obs_control):
-    n_test = len(obs_test)
-    n_control = len(obs_control)
-
-    return (np.var(obs_test) / n_test) + (np.var(obs_control) / n_control)
+    def var(self):
+        n_test = len(self.obs_test)
+        n_control = len(self.obs_control)
+        var_test = np.var(self.obs_test) / n_test
+        var_control = np.var(self.obs_control) / n_control
+        return var_test + var_control
