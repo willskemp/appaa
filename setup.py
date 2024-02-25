721ae59b542f0b457394a0d28dbd42ab5a579492
willskemp <willskemp@gmail.com>
2024-02-25 23:24:54 +0000

    move setup config to .cfg file

--- setup.py
+++ setup.py
@@ -5,16 +5,6 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name="appa",
-    version="1.0.0",
-    description="A Python package for analysing randomised experiments.",
-    author="Will Kemp",
-    author_email="",
-    install_requires=[
-        "numpy",
-        "pandas",
-    ],
-)
+setuptools.setup()
