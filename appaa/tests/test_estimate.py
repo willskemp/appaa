71646051a3da1e7b2fc0dbd5cf0c8e54b4f4cea3
willskemp <willskemp@gmail.com>
2024-02-26 14:49:02 +0000

    Update test_estimate.py

--- appaa/tests/test_estimate.py
+++ appaa/tests/test_estimate.py
@@ -14,7 +14,9 @@ def test_estimate(test_estimator: DifferenceInMeans = test_estimator):
     assert np.round(test_estimator.mean_Yt, 5) == 99.96336
     assert np.round(test_estimator.mean_Yc, 5) == 99.92445
 
+
 def test_variance(test_estimator: DifferenceInMeans = test_estimator):
-    test_estimator.variance()
+    var_estimate = test_estimator.variance()
     assert np.round(test_estimator.var_Yt, 5) == 3.87324
     assert np.round(test_estimator.var_Yc, 5) == 4.07519
+    assert np.round(var_estimate, 5) == 0.00795
