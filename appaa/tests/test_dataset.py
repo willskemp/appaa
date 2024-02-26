5ac4a8dba5e501ec92abe1fa4d34c7ab95961fed
willskemp <willskemp@gmail.com>
2024-02-26 12:03:41 +0000

    Updated test

--- appaa/tests/test_dataset.py
+++ appaa/tests/test_dataset.py
@@ -1,19 +1,15 @@
-from dataset import Dataset
+from appaa.dataset import Dataset
 import numpy as np
 import pandas as pd
 import os
 from pathlib import Path
 
-wd = Path.cwd()
-os.chdir(wd.parent)
-test_df = pd.read_csv('./data/test_data.csv')
-test_dataset = Dataset(test_df)
 
-
-def test_obs_to_array(test_dataset: Dataset):
+def test_obs_to_array():
+    wd = Path.cwd()
+    os.chdir(wd.parent)
+    test_df = pd.read_csv('test_data.csv')
+    test_dataset = Dataset(test_df)
     test_obs = test_dataset._to_obs_to_array(0)
     assert len(test_obs) == 1000
-    assert type(test_obs) is np.array
-
-
-test_obs_to_array(test_dataset)
+    assert type(test_obs) is np.ndarray
