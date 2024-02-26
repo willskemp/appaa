e6e6208a19aa7450562dcce7a56a107d45d3be82
willskemp <willskemp@gmail.com>
2024-02-26 14:36:42 +0000

    Update test_estimate.py

--- appaa/tests/test_estimate.py
+++ appaa/tests/test_estimate.py
@@ -1,2 +1,14 @@
-def test_true():
-    assert True
+from appaa.dataset import Dataset
+from appaa.estimate import DifferenceInMeans
+from appaa.data.read_test_data import load_data
+import numpy as np
+
+test_df = load_data()
+test_dataset = Dataset(test_df)
+test_estimator = DifferenceInMeans(test_dataset)
+
+
+def test_estimate(test_estimator: DifferenceInMeans = test_estimator):
+    test_estimator.estimate()
+    assert np.round(test_estimator.mean_Yt, 5) == 99.96336
+    assert np.round(test_estimator.mean_Yc, 5) == 99.92445
