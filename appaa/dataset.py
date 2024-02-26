5e8c59c0b7346470c33ff127087610017654c648
Liam Galbraith <liamgalbraith@gmail.com>
2024-02-26 10:01:01 +0000

    Updates typehints to use typing.Any

--- appaa/dataset.py
+++ appaa/dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import Any
 
 
 class Dataset:
@@ -6,8 +7,8 @@ class Dataset:
     def __init__(self, data: pd.DataFrame, version_col: str):
         self.data = data
 
-    def obs_test(self, id: any):
+    def obs_test(self, id: Any):
         return None
 
-    def obs_control(self, id: any):
+    def obs_control(self, id: Any):
         return None
