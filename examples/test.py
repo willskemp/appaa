8c563f0042a96fdce8e824bdac59f42b184ac7d2
willskemp <willskemp@gmail.com>
2024-02-26 20:01:24 +0000

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -3,11 +3,11 @@ from appaa.estimator import DifferenceInMeans
 from appaa.data.read_test_data import load_data
 
 df = load_data()
-df.head()
 
 estimator = DifferenceInMeans(alpha=0.05, beta=0.8)
 analyse = PostAnalysis(data=df,
                        test_ids=[1],
                        control_ids=[0],
                        estimator=estimator)
-analyse.results()
+output = analyse.results()
+print(output.head())
