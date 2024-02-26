a163c7c7353fd1e61088e8e6fbd2edaf7b55420f
willskemp <willskemp@gmail.com>
2024-02-26 14:21:06 +0000

    Update test to use new load_data function

--- appaa/data/read_test_data.py
+++ appaa/data/read_test_data.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 
-def get_data():
+def load_data():
     fname = os.path.join(os.path.dirname(__file__), 'test_data.csv')
     data = pd.read_csv(fname, index_col=0)
     return data
--- appaa/tests/test_dataset.py
+++ appaa/tests/test_dataset.py
@@ -1,12 +1,10 @@
 from appaa.dataset import Dataset
+from appaa.data.read_test_data import load_data
 import numpy as np
-import pandas as pd
 
 
 def test_obs_to_array():
-    import os
-    fname = os.path.join(os.path.dirname(__file__), 'test_data.csv')
-    test_df = pd.read_csv(fname, index_col=0)
+    test_df = load_data()
     test_dataset = Dataset(test_df)
     test_obs = test_dataset._obs_to_array(0)
     assert len(test_obs) == 1000
