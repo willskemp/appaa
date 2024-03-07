97eac45364471063d6be986197bf4324c2ce9fd2
willskemp <willskemp@gmail.com>
2024-03-07 10:51:31 +0000

    Update test

--- appaa/tests/test_analysis.py
+++ appaa/tests/test_analysis.py
@@ -1,16 +1,22 @@
-from appaa.analysis import Analysis
+from appaa.analysis import ExperimentResults
 from appaa.estimator import DifferenceInMeans
 from appaa.data.test import load_data
 import numpy as np
 
 test_df = load_data()
+test_df["variant"] = test_df.index
 test_estimator = DifferenceInMeans()
-test_analysis = Analysis(
-    data=test_df, test_ids=[1], control_ids=[0], estimator=test_estimator
+test_analysis = ExperimentResults(
+    data=test_df,
+    estimator=test_estimator,
+    randomisation_unit_col="variant",
+    metric_col="Y",
+    variant_col="variant",
 )
+test_analysis.comparisons(test_ids=[1], control_ids=[0])
 
 
-def test_obs_to_array(analysis: Analysis = test_analysis):
+def test_obs_to_array(analysis: ExperimentResults = test_analysis):
     test_obs = analysis._obs_to_array(0)
     assert len(test_obs) == 1000
     assert type(test_obs) is np.ndarray
