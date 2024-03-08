8ceb4053cc2c521275ce8d1db7de7540066c0fe1
willskemp <willskemp@gmail.com>
2024-03-08 14:41:57 +0000

    Add progress bar / fix chart

--- appaa/simulation.py
+++ appaa/simulation.py
@@ -24,20 +24,25 @@ class Simulation:
 
     def analyse(self, estimator: Estimator):
         self.simulated_analysis = pd.DataFrame(data=None)
-        for i in range(0, self.iterations):
-            tmp_df = self.simulated_data[self.simulated_data.iteration == i]
-            experiment = ExperimentResults(
-                data=tmp_df,
-                estimator=estimator,
-                metric_col="Y",
-                variant_col="variant",
-            )
-            test_cnt = self.sample_generator.test_cnt
-            control_cnt = self.sample_generator.control_cnt
-            test_ids = [i for i in range(0, test_cnt)]
-            control_ids = [i for i in range(test_cnt, test_cnt + control_cnt)]
-            experiment.comparisons(test_ids=test_ids, control_ids=control_ids)
-            results_df = experiment.estimate_results()
-            simulated_df = self.simulated_analysis
-            self.simulated_analysis = pd.concat([simulated_df, results_df])
+        with progressbar.ProgressBar(max_value=self.iterations) as bar:
+            for i in range(0, self.iterations):
+                tmp_df = self.simulated_data[
+                    self.simulated_data.iteration == i
+                    ]
+                experiment = ExperimentResults(
+                    data=tmp_df,
+                    estimator=estimator,
+                    metric_col="Y",
+                    variant_col="variant",
+                )
+                test_cnt = self.sample_generator.test_cnt
+                control_cnt = self.sample_generator.control_cnt
+                id_cnt = test_cnt + control_cnt
+                test_ids = [i for i in range(0, test_cnt)]
+                control_ids = [i for i in range(test_cnt, id_cnt)]
+                experiment.comparisons(test_ids, control_ids)
+                results_df = experiment.estimate_results()
+                simulated_df = self.simulated_analysis
+                self.simulated_analysis = pd.concat([simulated_df, results_df])
+                bar.update(i)
         return self.simulated_analysis
